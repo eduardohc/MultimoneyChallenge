d7851f8309427de2a3a6464dba7453c69cef3145
Eduardo Hernandez <lalohernandez93@gmail.com>
2023-07-18 15:36:58 -0600

    Create function and character view model to fetch data

---
+++ MultimoneyCodeChallenge/ViewModel/CharacterListViewModel.swift
@@ -0,0 +1,45 @@
+//
+//  CharacterListViewModel.swift
+//  MultimoneyCodeChallenge
+//
+//  Created by Eduardo Hernandez on 18/07/23.
+//
+
+import Foundation
+import MultimoneyAPI
+
+class CharacterListViewModel: ObservableObject {
+    @Published var characters: [CharacterResultViewModel] = []
+    
+    private let networkService: Network
+    
+    init(networkService: Network) {
+        self.networkService = networkService
+    }
+    
+    func fetchCharacters() {
+        networkService.fetchCharacters { result in
+            switch result {
+            case .success(let characters):
+                DispatchQueue.main.async {
+                    self.characters = characters.map(CharacterResultViewModel.init)
+                }
+            case .failure(let error):
+                // Handle error
+                print("Error: \(error.localizedDescription)")
+            }
+        }
+    }
+}
+
+struct CharacterResultViewModel {
+    fileprivate let characterResult: GetCharacterListQuery.Data.Characters.Result?
+    
+    var id: ID {
+        characterResult?.id ?? ""
+    }
+    
+    var name: String {
+        characterResult?.name ?? ""
+    }
+}

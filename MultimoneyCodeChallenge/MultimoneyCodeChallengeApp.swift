780fd745f73d541e522e5d937522d14fc55b5ba5
Eduardo Hernandez <lalohernandez93@gmail.com>
2023-07-19 10:59:45 -0600

    Add character list view to launch in main view

--- MultimoneyCodeChallenge/MultimoneyCodeChallengeApp.swift
+++ MultimoneyCodeChallenge/MultimoneyCodeChallengeApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct MultimoneyCodeChallengeApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            CharacterList(characterListVM: CharacterListViewModel(networkService: Network.shared))
         }
     }
 }
